68427581a1dde522689ee940732eef49c7d91315
Dimas Fahmi <105398559+dimas-fahmi@users.noreply.github.com>
2025-04-13 22:27:47 +0700

    structurize project's folder

---
+++ app/(public)/page.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+import React from 'react';
+
+const LandingPage = () => {
+    return (
+        <div>
+            LandingPage
+        </div>
+    );
+};
+
+export default LandingPage;
