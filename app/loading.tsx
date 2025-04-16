931ac8670adf2eaa51d93fc5694f2d6d3b602f60
Dimas Fahmi <105398559+dimas-fahmi@users.noreply.github.com>
2025-04-16 12:02:04 +0700

    add loader component

---
+++ app/loading.tsx
@@ -0,0 +1,8 @@
+import MainLoader from "@/src/ui/components/tusktask/animation/MainLoader";
+import React from "react";
+
+const Loading = () => {
+  return <MainLoader />;
+};
+
+export default Loading;
