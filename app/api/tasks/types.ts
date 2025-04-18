40969702ca89cc435a5df17138b3d762aaa874e5
Dimas Fahmi <105398559+dimas-fahmi@users.noreply.github.com>
2025-04-18 14:01:54 +0700

    fix filter type

--- app/api/tasks/types.ts
+++ app/api/tasks/types.ts
@@ -1,20 +1,8 @@
 import { TaskInsertType, TaskType } from "@/src/db/schema/tasks";
 
-type HasDuplicate<
-  T extends readonly any[],
-  Acc extends any[] = [],
-> = T extends [infer Head, ...infer Tail]
-  ? Head extends Acc[number]
-    ? true
-    : HasDuplicate<Tail, [...Acc, Head]>
-  : false;
-
-type AssertNoDuplicates<T extends readonly any[]> =
-  HasDuplicate<T> extends true ? ["Error: Duplicates found in array", T] : T;
-
 export interface TasksGetApiRequest extends Partial<TaskType> {}
 
-export const filterFields: AssertNoDuplicates<(keyof TasksGetApiRequest)[]> = [
+export const filterFields: (keyof TasksGetApiRequest)[] = [
   "completedAt",
   "completedById",
   "createdAt",
