23b080202b3afc5a7bdfd24806fce310f62fa9e4
Dimas Fahmi <105398559+dimas-fahmi@users.noreply.github.com>
2025-05-25 12:57:34 +0700

    seo: add title to teams page metadata

--- app/dashboard/teams/page.tsx
+++ app/dashboard/teams/page.tsx
@@ -1,5 +1,10 @@
 import React from "react";
 import TeamsPageIndex from "./TeamsPageIndex";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Teams | TuskTask",
+};
 
 const TeamsPage = () => {
   return <TeamsPageIndex />;
