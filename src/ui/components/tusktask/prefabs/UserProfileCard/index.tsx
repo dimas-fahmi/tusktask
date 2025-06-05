d5c611abc7e3e472cae7f097e146284e487b4092
Dimas Fahmi <105398559+dimas-fahmi@users.noreply.github.com>
2025-06-05 14:27:05 +0700

    wip: partial UserProfileCard implementation

--- src/ui/components/tusktask/prefabs/UserProfileCard/index.tsx
+++ src/ui/components/tusktask/prefabs/UserProfileCard/index.tsx
@@ -4,33 +4,89 @@ import usePersonalContext from "@/src/lib/tusktask/hooks/context/usePersonalCont
 import { DEFAULT_AVATAR } from "@/src/lib/tusktask/constants/configs";
 import { truncateText } from "@/src/lib/tusktask/utils/truncateText";
 import { signOut } from "next-auth/react";
+import { getUserInitials } from "@/src/lib/tusktask/utils/getUserInitials";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "../../../shadcn/ui/popover";
+import {
+  Bell,
+  Ellipsis,
+  GalleryVertical,
+  LogOut,
+  MessageCircle,
+  Settings,
+} from "lucide-react";
+import { Button } from "../../../shadcn/ui/button";
+import PopoverAction from "../Popover/PopoverAction";
+import { Separator } from "../../../shadcn/ui/separator";
+import useNotificationContext from "@/src/lib/tusktask/hooks/context/useNotificationContext";
 
 const UserProfileCard = () => {
   // Pull personal data
   const { personal } = usePersonalContext();
 
+  // Pull setters from Notifications
+  const { setNotificationsDialogOpen } = useNotificationContext();
+
   return (
     // TODO : Finish popover menu
-    <div
-      className="flex gap-3 bg-sidebar-bakcground hover:bg-accent text-sidebar-foreground hover:text-accent-foreground transition-all duration-300 hover:p-2 hover:border rounded-md cursor-pointer"
-      onClick={() => signOut()}
-    >
+    <div className="flex gap-3 bg-sidebar-bakcground hover:bg-accent text-sidebar-foreground hover:text-accent-foreground transition-all duration-300 hover:p-2 hover:border rounded-md cursor-pointer">
       {/* Avatar */}
       <div>
         <Avatar className="w-14 h-14">
           <AvatarImage src={personal?.image ?? DEFAULT_AVATAR} />
-          <AvatarFallback>DF</AvatarFallback>
+          <AvatarFallback>{getUserInitials(personal?.name)}</AvatarFallback>
         </Avatar>
       </div>
 
       {/* Name & Username */}
-      <div className="flex-grow flex items-center overflow-hidden truncate text-ellipsis">
+      <div className="flex-grow flex items-center overflow-hidden truncate text-ellipsis justify-between">
         <div>
           <h1 className="font-semibold text-sm">
-            {truncateText("Dimas Fahmi Pebrian", 3)}
+            {truncateText(personal?.name ?? "", 5)}
           </h1>
-          <p className="text-xs">dimas-fahmi</p>
+          <p className="text-xs">{personal?.username}</p>
         </div>
+
+        <Popover>
+          <PopoverTrigger asChild>
+            <Button variant="empty" size={"sm"}>
+              <Ellipsis />
+            </Button>
+          </PopoverTrigger>
+          <PopoverContent className="p-1">
+            <PopoverAction
+              Icon={GalleryVertical}
+              title="Open profile"
+              action={() => {}}
+            />
+            <Separator />
+            <PopoverAction
+              Icon={MessageCircle}
+              title="Messages"
+              action={() => {}}
+            />
+            <PopoverAction
+              Icon={Bell}
+              title="Notifications"
+              action={() => {
+                setNotificationsDialogOpen(true);
+              }}
+            />
+            <PopoverAction Icon={Settings} title="Settings" action={() => {}} />
+            <Separator />
+            <PopoverAction
+              Icon={LogOut}
+              variant="destructive"
+              title="Log Out"
+              action={() => {
+                signOut();
+              }}
+            />
+          </PopoverContent>
+        </Popover>
       </div>
     </div>
   );
