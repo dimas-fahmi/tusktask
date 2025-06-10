08bd941acfd9bbe7f1b39a1ec0ddc37c9145b657
Dimas Fahmi <105398559+dimas-fahmi@users.noreply.github.com>
2025-06-10 17:53:12 +0700

    Add receivePromotion enum to field type on nots table
    Added new enum type 'receivePromotion' to support promotion notification if user is being promoted

--- src/db/schema/notifications.ts
+++ src/db/schema/notifications.ts
@@ -28,6 +28,7 @@ export const notifications = pgTable(
         "assignNotification", // when someone request other user to claim a task
         "reminder", // reminder notification for tasks deadline
         "system", // system broadcast
+        "receivePromotion",
       ],
     }).notNull(),
     category: text("category", {
