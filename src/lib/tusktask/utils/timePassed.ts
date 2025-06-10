4047b6f237e3ba2cfd437451cb51352b9c8fbf78
Dimas Fahmi <105398559+dimas-fahmi@users.noreply.github.com>
2025-06-10 23:26:39 +0700

    fix: prevent minus count
    Return 'Just now' instead of -1 seconds

--- src/lib/tusktask/utils/timePassed.ts
+++ src/lib/tusktask/utils/timePassed.ts
@@ -24,6 +24,10 @@ export function timePassed(date: Date | string | undefined | null): string {
   } else if (minutes > 0) {
     return `${minutes} minute${minutes > 1 ? "s" : ""} ago`;
   } else {
-    return `${seconds} second${seconds !== 1 ? "s" : ""} ago`;
+    if (seconds < 1) {
+      return "Just now";
+    } else {
+      return `${seconds} second${seconds !== 1 ? "s" : ""} ago`;
+    }
   }
 }
