2b98e4c8d841cb231a21b6a674d9d6917c8d6645
dimasfahmi <dimasfahmipebrian@gmail.com>
2025-06-23 13:35:29 +0700

    add caching to improve performance

--- src/lib/tusktask/server/fetchers/fetchTaskData.ts
+++ src/lib/tusktask/server/fetchers/fetchTaskData.ts
@@ -1,15 +1,27 @@
 import { db } from "@/src/db";
 import { constructDetailTaskQuery } from "../../queries/detailTask";
 import { DetailTask } from "@/src/types/task";
+import { unstable_cache } from "next/cache";
 
 export interface DetailTaskKey {
   keys: string[];
   taskId: string;
 }
 
-export async function fetchTaskData(
-  id: string
-): Promise<DetailTask | undefined> {
-  const query = constructDetailTaskQuery(id);
-  return (await db.query.tasks.findFirst(query)) as DetailTask;
-}
+export const fetchTaskData = async (id: string) => {
+  const result = unstable_cache(
+    async (id: string): Promise<DetailTask | undefined> => {
+      const query = constructDetailTaskQuery(id);
+      const result = await db.query.tasks.findFirst(query);
+      return result as DetailTask | undefined;
+    },
+
+    [`task`, id],
+    {
+      revalidate: false,
+      tags: [`task`, `task:${id}`],
+    }
+  );
+
+  return result(id);
+};
