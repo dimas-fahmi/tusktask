06f25b8ab5ecc16b06ca2be8bc77df4232a05482
dimasfahmi <dimasfahmipebrian@gmail.com>
2025-07-02 06:44:46 +0700

    refactor: simplify error handling in fetchConversations function

--- src/lib/tusktask/fetchers/fetchConversations.ts
+++ src/lib/tusktask/fetchers/fetchConversations.ts
@@ -2,19 +2,15 @@ import { ConversationsGetResponse } from "@/app/api/conversations/get";
 
 export const fetchConversations =
   async (): Promise<ConversationsGetResponse> => {
-    try {
-      const response = await fetch(`/api/conversations`, {
-        method: "GET",
-      });
+    const response = await fetch(`/api/conversations`, {
+      method: "GET",
+    });
 
-      const data = await response.json();
+    const data = await response.json();
 
-      if (!response.ok) {
-        throw data;
-      }
-
-      return data;
-    } catch (error) {
-      throw error;
+    if (!response.ok) {
+      throw data;
     }
+
+    return data;
   };
