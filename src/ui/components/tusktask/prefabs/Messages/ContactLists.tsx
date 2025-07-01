5b24a8928919e7032e24c1a496be2da58d3a2506
dimasfahmi <dimasfahmipebrian@gmail.com>
2025-07-01 19:11:59 +0700

    chore: remove unused imports from ContactLists component

--- src/ui/components/tusktask/prefabs/Messages/ContactLists.tsx
+++ src/ui/components/tusktask/prefabs/Messages/ContactLists.tsx
@@ -1,7 +1,5 @@
 import { AnimatePresence } from "motion/react";
 import React from "react";
-import { motion } from "motion/react";
-import { contacts, contactVariants } from "./variants";
 import useChatContext from "@/src/lib/tusktask/hooks/context/useChatContext";
 import RoomCard from "./RoomCard";
 
