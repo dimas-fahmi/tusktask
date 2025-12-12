5d64f9cbdc03c78de6631973e6d9323581b16750
dimas-fahmi <dimasfahmipebrian@gmail.com>
2025-12-13 02:41:53 +0700

    chore: add condition to only fetch when session is exist

--- src/lib/queries/hooks/useGetSelfProfile.ts
+++ src/lib/queries/hooks/useGetSelfProfile.ts
@@ -1,10 +1,13 @@
 import { useQuery } from "@tanstack/react-query";
+import { authClient } from "../../auth/client";
 import { queryIndex } from "..";
 
 export const useGetSelfProfile = () => {
   const { queryOptions } = queryIndex.self.profile();
+  const { data: session } = authClient.useSession();
 
   return useQuery({
+    enabled: !!session,
     ...queryOptions,
   });
 };
