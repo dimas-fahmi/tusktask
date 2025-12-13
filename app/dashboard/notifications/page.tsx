2a265ca2a8ae91358dae01e0352e86c9d5b3b863
dimas-fahmi <dimasfahmipebrian@gmail.com>
2025-12-14 01:13:44 +0700

    scaffod: notifications page

--- app/dashboard/notifications/page.tsx
+++ app/dashboard/notifications/page.tsx
@@ -5,7 +5,25 @@ export const metadata: Metadata = {
 };
 
 const NotificationsPage = () => {
-  return <div>NotificationsPage</div>;
+  return (
+    <div className="space-y-6">
+      <header>
+        <div>
+          <h1 className="text-4xl font-semibold">Notifications</h1>
+          <p className="text-sm font-light opacity-70">
+            Regularly review your notifications to stay updated on current
+            events
+          </p>
+        </div>
+      </header>
+
+      <div>
+        <span className="text-xs font-light opacity-70">
+          No unread notification, good job!
+        </span>
+      </div>
+    </div>
+  );
 };
 
 export default NotificationsPage;
