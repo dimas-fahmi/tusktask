df2439bef7917603b290f126b85057d0d034ccf2
dimas-fahmi <dimasfahmipebrian@gmail.com>
2025-12-14 14:06:41 +0700

    fix: add size to icon and aria-labels

--- app/dashboard/tasks/components/Controller.tsx
+++ app/dashboard/tasks/components/Controller.tsx
@@ -6,11 +6,11 @@ const Controller = () => {
     <div className="flex items-center justify-between">
       {/* Buttons */}
       <div className="flex items-center gap-2">
-        <Button size={"sm"} variant={"outline"}>
-          <Funnel />
+        <Button size={"sm"} variant={"outline"} aria-label="Filter tasks">
+          <Funnel className="w-4 h-4" />
         </Button>
-        <Button size={"sm"} variant={"outline"}>
-          <Search />
+        <Button size={"sm"} variant={"outline"} aria-label="Search tasks">
+          <Search className="w-4 h-4" />
         </Button>
       </div>
     </div>
