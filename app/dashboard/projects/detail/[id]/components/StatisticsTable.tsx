abee2f1cdb9825984a0d002fd24b7901a467abe4
dimas-fahmi <dimasfahmipebrian@gmail.com>
2025-12-16 04:56:28 +0700

    scaffold: Project Detail Page

---
+++ app/dashboard/projects/detail/[id]/components/StatisticsTable.tsx
@@ -0,0 +1,55 @@
+"use client";
+
+import { useQuery } from "@tanstack/react-query";
+import type { ExtendedProjectType } from "@/src/lib/app/app";
+import { queryIndex } from "@/src/lib/queries";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableRow,
+} from "@/src/ui/shadcn/components/ui/table";
+
+const StatisticsTable = ({ project }: { project?: ExtendedProjectType }) => {
+  const memberships = project?.memberships;
+
+  const tasksQuery = queryIndex.self.tasks({
+    projectId: project?.id,
+  });
+  const { data: tasksQueryResult } = useQuery({
+    ...tasksQuery.queryOptions,
+    staleTime: 1000 * 60 * 2,
+    gcTime: 1000 * 60 * 10,
+    enabled: !!project,
+  });
+
+  return (
+    <div>
+      <Table className="text-sm font-light">
+        <TableBody>
+          {/* Members Count */}
+          <TableRow>
+            <TableCell>Members</TableCell>
+            <TableCell>{memberships?.length || "Unknown"}</TableCell>
+          </TableRow>
+
+          {/* Total Tasks Count */}
+          <TableRow>
+            <TableCell>Tasks Created</TableCell>
+            <TableCell>
+              {tasksQueryResult?.result?.totalResults || "Unknown"}
+            </TableCell>
+          </TableRow>
+
+          {/* Active Tasks Count */}
+          <TableRow>
+            <TableCell>Tasks Created</TableCell>
+            <TableCell>{0}</TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
+    </div>
+  );
+};
+
+export default StatisticsTable;
---
+++ src/ui/components/ui/StackedAvatars/index.tsx
@@ -0,0 +1,33 @@
+import type { SanitizedUserType } from "@/src/lib/app/app";
+import { NO_IMAGE_FALLBACK_SQUARE } from "@/src/lib/app/configs";
+import {
+  Avatar,
+  AvatarFallback,
+  AvatarImage,
+} from "@/src/ui/shadcn/components/ui/avatar";
+
+const AvatarButton = ({ user }: { user: SanitizedUserType }) => {
+  return (
+    <button type="button">
+      <Avatar>
+        <AvatarImage
+          src={user?.image || NO_IMAGE_FALLBACK_SQUARE}
+          alt={`@${user?.username || user?.name}`}
+        />
+        <AvatarFallback>CN</AvatarFallback>
+      </Avatar>
+    </button>
+  );
+};
+
+const StackedAvatars = ({ users }: { users: SanitizedUserType[] }) => {
+  return (
+    <div className="flex -space-x-2">
+      {users?.map((user) => (
+        <AvatarButton key={user?.id || crypto.randomUUID()} user={user} />
+      ))}
+    </div>
+  );
+};
+
+export default StackedAvatars;
