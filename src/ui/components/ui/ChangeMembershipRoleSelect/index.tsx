ac572af1d1fc7457f29ce33a796159c98030e32b
dimas-fahmi <dimasfahmipebrian@gmail.com>
2025-12-23 05:11:06 +0700

    feat: finalized ChangeMembershipRoleSelect component

--- src/ui/components/ui/ChangeMembershipRoleSelect/index.tsx
+++ src/ui/components/ui/ChangeMembershipRoleSelect/index.tsx
@@ -1,7 +1,8 @@
 "use client";
 
 import { useQuery, useQueryClient } from "@tanstack/react-query";
-import { ChevronDown } from "lucide-react";
+import { ChevronDown, Loader } from "lucide-react";
+import { useRef, useState } from "react";
 import type { ProjectMembershipType } from "@/src/db/schema/project";
 import {
   PROJECT_MEMBERSHIP_ROLE_HIERARCHY,
@@ -11,12 +12,18 @@ import {
 import { authClient } from "@/src/lib/auth/client";
 import { queryIndex } from "@/src/lib/queries";
 import { useUpdateMembership } from "@/src/lib/queries/hooks/useUpdateMembership";
+import { useNotificationStore } from "@/src/lib/stores/notification";
+import type {
+  NotificationMessageType,
+  ProjectMembershipRoleType,
+} from "@/src/lib/zod/notification";
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "@/src/ui/shadcn/components/ui/dropdown-menu";
+import CommitMessageDialog from "../CommitMessageDialog";
 
 export interface ChangeMembershipRoleSelectProps {
   membership: ProjectMembershipType;
@@ -24,8 +31,16 @@ export interface ChangeMembershipRoleSelectProps {
 const ChangeMembershipRoleSelect = ({
   membership,
 }: ChangeMembershipRoleSelectProps) => {
+  // Pull triggers from notification provider
+  const { triggerToast } = useNotificationStore();
+
+  // Commit message dialog state
+  const [CMDOpen, setCMDOpen] = useState(false);
+
+  // Fetching current user session
   const { data: session } = authClient.useSession();
   const userId = session?.user?.id;
+
   // Fetch current user's membership
   const membershipQuery = queryIndex.project.memberships({
     projectId: membership.projectId,
@@ -54,56 +69,113 @@ const ChangeMembershipRoleSelect = ({
 
   const queryClient = useQueryClient();
 
-  return (
-    <DropdownMenu>
-      <DropdownMenuTrigger
-        className="w-full transition-all duration-300 hover:scale-95 active:scale-90 capitalize text-left text-sm font-light px-4 py-2 border rounded-lg flex items-center justify-between"
-        disabled={
-          !currentUserPermissions?.manageMembership ||
-          isLoadingCurrentUserMembership
-        }
-      >
-        {membership.type}
+  const newRoleRef = useRef<ProjectMembershipRoleType | null>(null);
+  const handlePreSave = () => {
+    if (!newRoleRef.current) {
+      return;
+    }
 
-        <ChevronDown className="w-4 h-4" />
-      </DropdownMenuTrigger>
-      <DropdownMenuContent>
-        {PROJECT_MEMBERSHIP_ROLES.map((role) => (
-          <DropdownMenuItem
-            className="capitalize"
-            key={role}
-            id={role}
-            disabled={
-              (currentUserHierarchy >=
-                PROJECT_MEMBERSHIP_ROLE_HIERARCHY[role] &&
-                !isCurrentUserSupreme) ||
-              isUpdatingMembership
-            }
-            onClick={() => {
-              updateMembership(
-                {
-                  projectId: membership.projectId,
-                  userId: membership.userId,
-                  newValues: {
-                    type: role,
-                  },
-                },
-                {
-                  onSettled: () => {
-                    queryClient.invalidateQueries({
-                      queryKey: ["project", "memberships"],
-                      exact: false,
-                    });
-                  },
-                },
-              );
-            }}
-          >
-            {role}
-          </DropdownMenuItem>
-        ))}
-      </DropdownMenuContent>
-    </DropdownMenu>
+    setCMDOpen(true);
+  };
+
+  const handleFinalSave = (message: NotificationMessageType) => {
+    if (!newRoleRef.current) return;
+    updateMembership(
+      {
+        projectId: membership.projectId,
+        userId: membership.userId,
+        newValues: {
+          type: newRoleRef.current,
+        },
+        message,
+      },
+      {
+        onSuccess: () => {
+          triggerToast(
+            "Changes Saved Successfully",
+            {
+              description: `User's role changed from ${membership.type} to ${newRoleRef.current}`,
+            },
+            "success",
+          );
+        },
+        onError: () => {
+          triggerToast(
+            "Failed to Save Changes",
+            {
+              description: `Failed to update User's role from ${membership.type} to ${newRoleRef.current}`,
+            },
+            "error",
+          );
+        },
+        onSettled: () => {
+          newRoleRef.current = null;
+          queryClient.invalidateQueries({
+            queryKey: ["project", "memberships"],
+            exact: false,
+          });
+          setCMDOpen(false);
+        },
+      },
+    );
+  };
+
+  return (
+    <>
+      {/* Dropdown Menu */}
+      <DropdownMenu>
+        <DropdownMenuTrigger
+          className="w-full transition-all duration-300 hover:scale-95 capitalize text-left text-sm font-light px-4 py-2 border rounded-lg disabled:pointer-events-none flex items-center justify-between"
+          disabled={
+            !currentUserPermissions?.manageMembership ||
+            isLoadingCurrentUserMembership ||
+            isUpdatingMembership
+          }
+        >
+          {!isUpdatingMembership ? (
+            <>
+              {membership.type}
+              <ChevronDown className="w-4 h-4" />
+            </>
+          ) : (
+            <>
+              <span>Saving Changes</span>
+              <Loader className="w-4 h-4 animate-spin" />
+            </>
+          )}
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          {PROJECT_MEMBERSHIP_ROLES.map((role) => (
+            <DropdownMenuItem
+              className="capitalize"
+              key={role}
+              id={role}
+              disabled={
+                (currentUserHierarchy >=
+                  PROJECT_MEMBERSHIP_ROLE_HIERARCHY[role] &&
+                  !isCurrentUserSupreme) ||
+                isUpdatingMembership
+              }
+              onClick={() => {
+                newRoleRef.current = role;
+                handlePreSave();
+              }}
+            >
+              {role}
+            </DropdownMenuItem>
+          ))}
+        </DropdownMenuContent>
+      </DropdownMenu>
+
+      {/* Commit Message Dialog */}
+      <CommitMessageDialog
+        open={CMDOpen}
+        onOpenChange={setCMDOpen}
+        onConfirm={handleFinalSave}
+        isPending={isUpdatingMembership}
+        description={`Explain why would you change this user's role from ${membership.type} to ${newRoleRef.current}`}
+      />
+    </>
   );
 };
 
