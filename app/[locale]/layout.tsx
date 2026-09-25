8b8aa1e743591109554a2f051751e501baee1650
Dimas Fahmi <dimasfahmipebrian@gmail.com>
2026-09-25 08:25:57 +0700

    feat(layout): add Toaster component to LocaleLayout for notifications

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,11 +1,19 @@
 import { NextIntlClientProvider } from "next-intl";
 import type React from "react";
+import { Toaster } from "@/src/ui/shadcn/components/ui/toast";
 
 const LocaleLayout = ({
   children,
 }: {
   children: Readonly<React.ReactNode>;
 }) => {
-  return <NextIntlClientProvider>{children}</NextIntlClientProvider>;
+  return (
+    <NextIntlClientProvider>
+      {children}
+
+      {/* TOASTER */}
+      <Toaster />
+    </NextIntlClientProvider>
+  );
 };
 export default LocaleLayout;
