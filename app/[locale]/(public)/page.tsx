6c761f7509506273a57f5b025339808cbd3b1ae2
Dimas Fahmi <dimasfahmipebrian@gmail.com>
2026-09-25 20:17:14 +0700

    chore: remove debugging and testing leftover

--- app/[locale]/(public)/page.tsx
+++ app/[locale]/(public)/page.tsx
@@ -1,23 +1,4 @@
-import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
-import { Suspense } from "react";
-import { serverGetQueryClient, serverTRPC } from "@/src/lib/trpc/server/caller";
-import HomepageIndex from "./HomepageIndex";
-
 const HomePage = async () => {
-  const qc = serverGetQueryClient();
-
-  void qc.query({
-    ...serverTRPC.truth.queryOptions({
-      name: "Dimas",
-    }),
-  });
-
-  return (
-    <HydrationBoundary state={dehydrate(qc)}>
-      <Suspense fallback={<div>Loading...</div>}>
-        <HomepageIndex />
-      </Suspense>
-    </HydrationBoundary>
-  );
+  return <div>Homepage</div>;
 };
 export default HomePage;
