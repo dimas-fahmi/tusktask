60ee0594be57a1cb79a902a3378b17714afb5c33
Dimas Fahmi <dimasfahmipebrian@gmail.com>
2026-09-25 22:01:58 +0700

    feat(auth): add registration completion alert and toast notification for onboarding

--- src/auth/clientRouteGuard.tsx
+++ src/auth/clientRouteGuard.tsx
@@ -1,18 +1,22 @@
 "use client";
 
+import Cookies from "js-cookie";
 import { usePathname, useRouter } from "next/navigation";
+import { useTranslations } from "next-intl";
 import type React from "react";
 import { useEffect } from "react";
 import route from "../app/route";
 import { isGuestOnlyRoute, isProtectedRoute } from "../app/route/constraint";
 import { useMyData } from "../hooks/useMyData";
 import { stripLocaleFromPathname } from "../i18n";
+import { Toaster } from "../utils/clientOnly/triggerToast";
 
 const clientRouteGuard = ({
   children,
 }: {
   children: Readonly<React.ReactNode>;
 }) => {
+  const t = useTranslations();
   const { data: myData, isFetching } = useMyData();
 
   const router = useRouter();
@@ -26,6 +30,21 @@ const clientRouteGuard = ({
       myData.registrationStep !== "completed" &&
       pathname !== route.onboarding()
     ) {
+      const toast = new Toaster({
+        id: "registration-first",
+        title: t("alert.finish_registration_first.title"),
+        description: t("alert.finish_registration_first.desc"),
+        type: "warning",
+      });
+
+      const isFirst = !!Cookies.get("registration-first");
+
+      if (!isFirst) {
+        Cookies.set("registration-first", "1");
+      } else {
+        toast.trigger();
+      }
+
       return router.push(route.onboarding());
     }
 
@@ -36,7 +55,7 @@ const clientRouteGuard = ({
     if (isGuestOnlyRoute(pathname) && myData) {
       return router.push(route.app());
     }
-  }, [myData, isFetching, router, pathname]);
+  }, [myData, isFetching, router, pathname, t]);
 
   return children;
 };
