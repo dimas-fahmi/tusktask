264f853a98094e2efe9052969e01af00f71aa064
Dimas Fahmi <dimasfahmipebrian@gmail.com>
2026-09-27 02:18:31 +0700

    feat(registration): add metadata generation and localization for registration page

--- app/[locale]/(auth)/registration/page.tsx
+++ app/[locale]/(auth)/registration/page.tsx
@@ -1,4 +1,15 @@
+import type { Metadata } from "next";
+import { getTranslations } from "next-intl/server";
+import { getEnv } from "@/src/app/env";
 import RegistrationPageIndex from "./RegistrationPageIndex";
+
+export async function generateMetadata(): Promise<Metadata> {
+  const t = await getTranslations();
+
+  return {
+    title: `${t("page.registration.title")} | ${getEnv("NEXT_PUBLIC_APP_NAME")}`,
+  };
+}
 
 const RegistrationPage = () => {
   return <RegistrationPageIndex />;
